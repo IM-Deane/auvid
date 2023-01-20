20203d2fce34e9dee43848034e63dd3dcd455fe8
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-01-19 22:02:33 -0700

    Added project boilerplate and audio recorder

---
+++ components/AudioRecorder.tsx
@@ -0,0 +1,35 @@
+import { invokeSaveAsDialog } from "recordrtc";
+
+import { useRecorderPermission } from "../hooks";
+
+function AudioRecorder() {
+	const recorder = useRecorderPermission("audio");
+
+	const startRecording = async () => {
+		recorder.startRecording();
+	};
+	const stopRecording = async () => {
+		await recorder.stopRecording();
+		let blob = await recorder.getBlob();
+
+		const formData = new FormData();
+		formData.append("key", blob);
+
+		// send to server
+
+		// xhr.send(formData);
+
+		invokeSaveAsDialog(blob, "recording.wav");
+	};
+
+	return (
+		<div>
+			<h1>Audio Recorder</h1>
+
+			<button onClick={startRecording}> Start recording</button>
+			<button onClick={stopRecording}> Stop recording</button>
+		</div>
+	);
+}
+
+export default AudioRecorder;
