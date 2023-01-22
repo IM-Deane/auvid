e8b439b69e8e8a7db9496dbb7cf08e397ffd4274
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-01-21 23:59:29 -0700

    Moved upload logic to its own page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,11 @@
-import { useState } from "react";
-
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 import Layout from "../components/Layout";
 
-import FileUploader from "../components/FileUploader";
-import Alert from "../components/Alert";
-
 const IndexPage = () => {
-	const [showAlert, setShowAlert] = useState(false);
-	const [error, setError] = useState({
-		status: false,
-		message: "",
-	});
-
 	const session = useSession();
 	const supabase = useSupabaseClient();
-
-	const handleUploadResult = (data) => {
-		if (!data) {
-			setError({ status: true, message: "Error uploading file" });
-		} else {
-			setError({ status: false, message: "" }); // reset error status
-		}
-
-		setShowAlert(true);
-	};
-
-	const handleAlertDismiss = () => setShowAlert(false);
 
 	return !session ? (
 		<Auth
@@ -41,14 +18,6 @@ const IndexPage = () => {
 			<h1 className="text-2xl font-semibold text-gray-900">
 				Hello RustleAI 👋
 			</h1>
-			<FileUploader handleResult={handleUploadResult} />
-			{showAlert && (
-				<Alert
-					handleAlertDismiss={handleAlertDismiss}
-					text={error.status ? error.message : "File uploaded successfully"}
-					isError={error.status}
-				/>
-			)}
 		</Layout>
 	);
 };
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,11 +1,41 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { useState } from "react";
 
-const AudioUpload = () => (
-	<Layout title="Upload Audio | RustleAI">
-		<h1 className="text-2xl font-semibold text-gray-900">Upload Audio</h1>
-		<p>This page handles uploading audio to server</p>
-	</Layout>
-);
+import FileUploader from "../components/FileUploader";
+import Layout from "../components/Layout";
+import Alert from "../components/Alert";
+
+const AudioUpload = () => {
+	const [showAlert, setShowAlert] = useState(false);
+	const [error, setError] = useState({
+		status: false,
+		message: "",
+	});
+
+	const handleUploadResult = (data) => {
+		if (!data) {
+			setError({ status: true, message: "Error uploading file" });
+		} else {
+			setError({ status: false, message: "" }); // reset error status
+		}
+
+		setShowAlert(true);
+	};
+
+	const handleAlertDismiss = () => setShowAlert(false);
+
+	return (
+		<Layout title="Upload Audio | RustleAI">
+			<h1 className="text-2xl font-semibold text-gray-900">Add new audio</h1>
+			<FileUploader handleResult={handleUploadResult} />
+			{showAlert && (
+				<Alert
+					handleAlertDismiss={handleAlertDismiss}
+					text={error.status ? error.message : "File uploaded successfully"}
+					isError={error.status}
+				/>
+			)}
+		</Layout>
+	);
+};
 
 export default AudioUpload;
