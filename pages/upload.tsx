1d3cdc3ff18c55d5755674678671bf14a93d24a3
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-01-27 02:00:27 -0700

    File cleanup

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -62,67 +62,6 @@ const AudioUpload = () => {
 						setShowAlert={setShowAlert}
 						setError={setError}
 					/>
-
-					{/* <div>
-						<label
-							htmlFor="email"
-							className="block text-sm font-medium text-gray-700"
-						>
-							Filename:
-						</label>
-						<div className="mt-1">
-							<input
-								type="text"
-								name="filename"
-								id="filename"
-								value={uploadedFile}
-								onChange={handleUploadFileChange}
-								minLength={5}
-								maxLength={50}
-								className="block w-1/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-								aria-describedby="filename-input"
-							/>
-						</div>
-						<p className="mt-2 text-sm text-gray-500" id="filename-description">
-							A memorable name for your file
-						</p>
-					</div> */}
-
-					{/* <div className="mt-3">
-						<h2 className="text-xl my-6 font-semibold text-dark-900">
-							Transcribed Text:
-						</h2>
-						{summarizedText && (
-							<div className="my-4">
-								<h3 className="text-l font-semibold text-dark-900">TLDR:</h3>
-								<p className="text-lg text-black bg-yellow-200 p-3">
-									{summarizedText}
-								</p>
-							</div>
-						)}
-						<h3 className="text-xl font-semibold text-dark-900">Full Text:</h3>
-						<p className="text-xl text-black p-3">{transcribedText}</p>
-					</div> */}
-					{/* only show button if there's enough text to summarize  */}
-					{/* {transcribedText.length > 15 && (
-						<div className="mt-4">
-							<LoadingButton
-								isLoading={loading}
-								text={summarizedText ? "Try again" : "Summarize"}
-								loadingText="Summarizing..."
-								handleClick={handleSummarizeText}
-							/>
-						</div>
-					)}
-
-					<div className="mt-4">
-						<LoadingButton
-							isLoading={isSaving}
-							text="Save to notes"
-							loadingText="Saving..."
-							handleClick={handleSaveNotes}
-						/>
-					</div>*/}
 				</div>
 			)}
 		</Layout>
