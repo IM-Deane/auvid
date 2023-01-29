41d228babf6b6ff1b9a37b4066eeae600fba295d
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-01-29 02:59:38 -0700

    Added more methods to analytics service

--- utils/services/analytics-service.ts
+++ utils/services/analytics-service.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
+import { EventCountSearchParams } from "../../types/index";
+
 class AnalyticsService {
 	private service: any;
 
@@ -13,9 +15,29 @@ class AnalyticsService {
 	}
 
 	/**
+	 * Get all analytics events related to the user
+	 * @returns {AxiosResponse} response object containing all events
+	 */
+	getEvents = async (): Promise<AxiosResponse> => {
+		return await this.service.get("/");
+	};
+
+	/**
+	 * Get a count for the specified event(s) related to the user
+	 * @param {object} params object containing the event types to count
+	 * @returns {AxiosResponse} response object containing all events
+	 */
+	getEventCounts = async (
+		params: EventCountSearchParams
+	): Promise<AxiosResponse> => {
+		return await this.service.get("/search", { params });
+	};
+
+	/**
 	 * Creates a new transcription event in the database and
 	 * returns the event object in the response
 	 * @param {string} filename original name of the audio/video file being transcribed
+	 * @returns {AxiosResponse} response object containing the transcription event
 	 */
 	createTranscriptionEvent = async (
 		filename: string
