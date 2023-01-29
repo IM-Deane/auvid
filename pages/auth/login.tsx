7fd60c139623e90fa379eafd5dd1c2b182783cca
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-01-28 17:42:07 -0700

    Added custom login form

---
+++ pages/auth/login.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+
+import LoginForm from "../../components/LoginForm";
+
+function login() {
+	return (
+		<div>
+			<LoginForm />
+		</div>
+	);
+}
+
+export async function getServerSideProps(ctx) {
+	const supabase = createServerSupabaseClient(ctx);
+	const { data: session } = await supabase.auth.getSession();
+
+	if (session) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
+}
+
+export default login;
