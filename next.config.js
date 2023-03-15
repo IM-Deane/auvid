4895b0a47be91c0c1667b9fc293d0499a64035e2
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-03-14 21:29:12 -0600

    Fix broken image format

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   staticPageGenerationTimeout: 300,
   images: {
     domains: ['tailwindui.com', 'images.unsplash.com'],
-    formats: ['image/webp', 'image/jpeg', 'image/png', 'image/svg+xml'],
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
 }
