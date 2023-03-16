d6e67d2c81b78b506253d016b147e99f280ec8d7
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-03-15 20:19:18 -0600

    Added event_count migration for monthly views. Generated DB types

---
+++ supabase/migrations/20230316021550_add_monthly_event_count_view_per_user_and_function.sql
@@ -0,0 +1,60 @@
+set check_function_bodies = off;
+
+create or replace view "public"."events_per_user_current_month" as  SELECT events.profile_id,
+    count(*) AS event_count
+   FROM events
+  WHERE (events.created_at >= date_trunc('month'::text, (CURRENT_DATE)::timestamp with time zone))
+  GROUP BY events.profile_id;
+
+
+CREATE OR REPLACE FUNCTION public.get_current_month_event_count(profile_id uuid)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    event_count INTEGER;
+BEGIN
+    SELECT event_count
+    INTO event_count
+    FROM events_per_user_current_month
+    WHERE profile_id = get_current_month_event_count.profile_id;
+    
+    RETURN event_count;
+END;
+$function$
+;
+
+
+create policy "Give users access to own folder 1qipc1_0"
+on "storage"."objects"
+as permissive
+for select
+to authenticated
+using (((bucket_id = 'notes'::text) AND ((auth.uid())::text = (storage.foldername(name))[1])));
+
+
+create policy "Give users access to own folder 1qipc1_1"
+on "storage"."objects"
+as permissive
+for insert
+to authenticated
+with check (((bucket_id = 'notes'::text) AND ((auth.uid())::text = (storage.foldername(name))[1])));
+
+
+create policy "Give users access to own folder 1qipc1_2"
+on "storage"."objects"
+as permissive
+for update
+to authenticated
+using (((bucket_id = 'notes'::text) AND ((auth.uid())::text = (storage.foldername(name))[1])));
+
+
+create policy "Give users access to own folder 1qipc1_3"
+on "storage"."objects"
+as permissive
+for delete
+to authenticated
+using (((bucket_id = 'notes'::text) AND ((auth.uid())::text = (storage.foldername(name))[1])));
+
+
+
