aa119ff9517b9ebfba9fbd15eb1f397059a7f9f6
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-03-24 18:10:19 -0600

    Added client fetch to usage page

--- pages/account/usage.tsx
+++ pages/account/usage.tsx
@@ -1,16 +1,14 @@
-import { GetServerSidePropsContext } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-import type { FullUser } from '@/supabase/types/index'
-import type { Database } from '@/supabase/types/public'
+import useInternalAPI from '@/hooks/useInternalAPI'
 import { classNames, updateActiveSettingsTab } from '@/utils/index'
 import { ClockIcon, DocumentTextIcon } from '@heroicons/react/24/outline'
-import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import siteConfig from 'site.config'
 
 import Layout from '@/components/Layout'
 import AccountTabHeader from '@/components/Settings/AccountTabHeader'
+import LoadingSkeleton from '@/components/cards/LoadingSkeleton'
 
 const actions = [
   {
@@ -29,12 +27,37 @@ const actions = [
   }
 ]
 
-const UsagePage = ({ eventCount }: { user: FullUser; eventCount: number }) => {
+const UsagePage = () => {
   const router = useRouter()
   const tabs = updateActiveSettingsTab(router.pathname)
 
-  // TODO: pass user's current usage metrics to children
-  console.log(eventCount)
+  const { data, error, isLoading } = useInternalAPI('/api/events')
+
+  if (error) return error
+  if (!data || isLoading)
+    return (
+      <Layout title={`Loading billing data... | ${siteConfig.siteName}`}>
+        <div className='lg:px-8'>
+          <div className='mx-auto flex flex-col lg:max-w-4xl'>
+            <main className='flex-1'>
+              <div className='relative mx-auto max-w-4xl'>
+                <div className='pt-10 pb-16'>
+                  <div className='px-4 sm:px-6 lg:px-0'>
+                    <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
+                      Usage
+                    </h1>
+                  </div>
+                </div>
+                <LoadingSkeleton count={2} />
+              </div>
+            </main>
+          </div>
+        </div>
+      </Layout>
+    )
+
+  console.log('total life time events', data.events.data)
+  console.log('total monthly event count', data.monthlyEventCount.data)
 
   return (
     <Layout title={`Billing and usage | ${siteConfig.siteName}`}>
@@ -45,7 +68,7 @@ const UsagePage = ({ eventCount }: { user: FullUser; eventCount: number }) => {
               <div className='pt-10 pb-16'>
                 <div className='px-4 sm:px-6 lg:px-0'>
                   <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
-                    Billing
+                    Usage
                   </h1>
                 </div>
                 <AccountTabHeader tabs={tabs} />
@@ -96,30 +119,6 @@ const UsagePage = ({ eventCount }: { user: FullUser; eventCount: number }) => {
       </div>
     </Layout>
   )
-}
-
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  const supabase = createServerSupabaseClient<Database>(ctx)
-  const {
-    data: { user }
-  } = await supabase.auth.getUser()
-
-  const [profileData, eventData] = await Promise.all([
-    supabase.from('profiles').select('*').eq('id', user.id).single(),
-    supabase.rpc('get_current_month_event_count')
-  ])
-
-  const userData = {
-    ...user,
-    profile: { ...profileData }
-  }
-
-  return {
-    props: {
-      user: userData,
-      eventCount: eventData
-    }
-  }
 }
 
 export default UsagePage
