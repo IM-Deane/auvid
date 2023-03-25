f00b35bff45c9fbdd920d06a86936daed9f6c63d
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-03-25 10:03:08 -0600

    Add logo to password reset form

--- components/Auth/ResetPasswordForm.tsx
+++ components/Auth/ResetPasswordForm.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 
@@ -98,11 +99,13 @@ function ResetPasswordForm() {
   return (
     <div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8'>
       <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-        <img
-          className='mx-auto h-12 w-auto'
-          src={siteConfig.productBrand}
-          alt={`${siteConfig.siteName} logo`}
-        />
+        <div className='relative mx-auto h-12 w-12'>
+          <Image
+            src={siteConfig.productBrand}
+            alt={`${siteConfig.siteName} logo`}
+            fill
+          />
+        </div>
         <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
           Update your password
         </h2>
