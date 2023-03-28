2a6276f5ce35d83afe58cbce2e01d8d33e2767fb
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-03-28 15:18:52 -0600

    Updated note event  list styling

--- components/Dashboard/EventCountList/index.tsx
+++ components/Dashboard/EventCountList/index.tsx
@@ -33,52 +33,56 @@ function EventCountList() {
       </header>
       <dt className='mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3'>
         {eventCounts &&
-          Object.entries(eventCounts).map(([key, value]) => (
-            <div
-              key={key + Date.now().toLocaleString()}
-              className='relative overflow-hidden rounded-lg bg-white px-4 pt-5 pb-12 shadow sm:px-6 sm:pt-6'
-            >
-              <div>
-                <div className='absolute rounded-md bg-blue-500 p-3'>
-                  <EnvelopeOpenIcon
-                    className='h-6 w-6 text-white'
-                    aria-hidden='true'
-                  />
+          Object.entries(eventCounts)
+            .reverse()
+            .map(([key, value]) => (
+              <div
+                key={key + Date.now().toLocaleString()}
+                className='relative overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:px-6 sm:pt-6'
+              >
+                <div>
+                  <div className='absolute rounded-md bg-blue-500 p-3'>
+                    <EnvelopeOpenIcon
+                      className='h-6 w-6 text-white'
+                      aria-hidden='true'
+                    />
+                  </div>
+                  <p className='ml-16 truncate text-sm font-medium text-gray-500'>
+                    Total {key.charAt(0).toUpperCase() + key.slice(1)}
+                  </p>
                 </div>
-                <p className='ml-16 truncate text-sm font-medium text-gray-500'>
-                  Total {key.charAt(0).toUpperCase() + key.slice(1)}
-                </p>
-              </div>
-              <div className='ml-16 flex items-baseline pb-6 sm:pb-7'>
-                <p className='text-2xl font-semibold text-gray-900'>{value}</p>
-                {value != 0 && (
-                  <p
-                    className={classNames(
-                      value > 0 ? 'text-green-600' : 'text-red-600',
-                      'ml-2 flex items-baseline text-sm font-semibold'
-                    )}
-                  >
-                    {value > 0 ? (
-                      <ArrowUpIcon
-                        className='h-5 w-5 flex-shrink-0 self-center text-green-500'
-                        aria-hidden='true'
-                      />
-                    ) : (
-                      <ArrowDownIcon
-                        className='h-5 w-5 flex-shrink-0 self-center text-red-500'
-                        aria-hidden='true'
-                      />
-                    )}
-                    <span className='sr-only'>
-                      {' '}
-                      {value > 0 ? 'Increased' : 'Decreased'} by{' '}
-                    </span>
+                <div className='ml-16 flex items-baseline pb-6 sm:pb-7'>
+                  <p className='text-2xl font-semibold text-gray-900'>
                     {value}
                   </p>
-                )}
+                  {value != 0 && (
+                    <p
+                      className={classNames(
+                        value > 0 ? 'text-green-600' : 'text-red-600',
+                        'ml-2 flex items-baseline text-sm font-semibold'
+                      )}
+                    >
+                      {value > 0 ? (
+                        <ArrowUpIcon
+                          className='h-5 w-5 flex-shrink-0 self-center text-green-500'
+                          aria-hidden='true'
+                        />
+                      ) : (
+                        <ArrowDownIcon
+                          className='h-5 w-5 flex-shrink-0 self-center text-red-500'
+                          aria-hidden='true'
+                        />
+                      )}
+                      <span className='sr-only'>
+                        {' '}
+                        {value > 0 ? 'Increased' : 'Decreased'} by{' '}
+                      </span>
+                      {value}
+                    </p>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
       </dt>
     </>
   )
